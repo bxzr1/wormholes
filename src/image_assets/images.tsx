004b9e5fea4e0edc83fe3637252e9088875c2dc7
Christopher Stewart <chris.stewart.csv@gmail.com>
2023-09-24 14:22:03 -0700

    Added background images for space - TODO add more images, etc.

---
+++ src/image_assets/images.tsx
@@ -0,0 +1,9 @@
+export const space_1 = require('./space_1.png');
+export const space_2 = require('./space_2.png');
+export const space_3 = require('./space_3.png');
+
+export const spaceImages = [
+    space_1,
+    space_2,
+    space_3,
+]
---
+++ src/image_assets/index.tsx
@@ -0,0 +1,3 @@
+export const space1 = require('./space_1.png');
+export const space2 = require('./space_2.png');
+export const space3 = require('./space_2.png');
